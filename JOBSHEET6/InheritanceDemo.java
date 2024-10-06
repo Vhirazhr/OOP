f508dd1bd2c9f6a9032d03e9556ef9d788269171
Vhirazhr <fakhirazahra88@gmail.com>
2024-10-06 11:01:17 +0700

    oop/js4/2

--- JOBSHEET6/InheritanceDemo.java
+++ JOBSHEET6/InheritanceDemo.java
@@ -1,5 +1,11 @@
 public class InheritanceDemo {
     public static void main(String[] args) {
         Dosen dosen1 = new Dosen();
+        dosen1.name = "Yansy Ayuningtias";
+        dosen1.nip = "1234567890";
+        dosen1.gaji = 3000000;
+        dosen1.nidn = "1989432439";
+
+        System.out.println(dosen1.getInfo());
     }
 }
--- JOBSHEET6/Pegawai.java
+++ JOBSHEET6/Pegawai.java
@@ -1,5 +1,17 @@
 public class Pegawai {
+    public String nip;
+    public String name;
+    public double gaji;
+
     public Pegawai (){
         System.out.println("Object dari class Pegawai dibuat");
+    }
+
+    public String getInfo (){
+        String info = "";
+        info += "NIP : " + nip + "\n";
+        info += "Nama : " + name + "\n";
+        info += "Gaji : " + gaji + "\n";
+        return info;
     }
 }
