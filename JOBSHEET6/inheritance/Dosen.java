8ce6ea495b1dbbafa367d1bce5e8f66b4ec40fec
Vhirazhr <fakhirazahra88@gmail.com>
2024-10-06 16:46:02 +0700

    oop/js6/5

--- JOBSHEET6/inheritance/Dosen.java
+++ JOBSHEET6/inheritance/Dosen.java
@@ -9,12 +9,13 @@ public class Dosen extends Pegawai{
         System.out.println("Object dari class Dosen dibuat");
     }
 
+    public String getInfo(){
+        return "NIDN : " + this.nidn + "\n";
+    }
+
     public String getAllInfo () {
-        String info = "";
-        info += "Nama : " + super.name + "\n";
-        info += "NIP : " + super.nip + "\n";
-        info += "Gaji : " + super.gaji + "\n";
-        info += "NIDN : " + this.nidn + "\n";
+        String info = super.getInfo();
+        info += this.getInfo();
         return info;
     }
 }
